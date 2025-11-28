a2d876c0119e1ee8621905868115dc6a88e2b97f
Omar <omar.j.black@gmail.com>
2025-11-27 19:26:31 -0500

    pre-search functions

--- src/WeatherAPI.js
+++ src/WeatherAPI.js
@@ -3,7 +3,6 @@ import './weatherapi.css';
 
 function WeatherAPI({ city }) {
   const [weatherData, setWeatherData] = useState(null);
-  const [weatherForecastData, setWeatherForecastData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -32,34 +31,8 @@ function WeatherAPI({ city }) {
         setLoading(false);
       }
     };
-
-    const getWeatherForecast = async () => {
-      try {
-        setLoading(true);
-        console.log(weatherApiKey);
-        const weatherUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=Toronto&appid=${weatherApiKey}&units=metric`;
-
-        // Throw error if API call fails
-        const response = await fetch(weatherUrlForecast);
-        if (!response.ok) {
-          // TODO: See if there is a better error method
-          throw new Error(`Error: ${response.status}`)
-        }
-
-        const data = await response.json();
-        setWeatherForecastData(data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-
     getWeather();
-    getWeatherForecast();
   }, []);
-
 
 
   //TODO: Fix with something appropriate
@@ -82,42 +55,14 @@ function WeatherAPI({ city }) {
           <h4 className="center">{sunrise.toDateString()}</h4>
         </div>
 
-        <div className="widget-right">
-
-          <div className="widget-right-top">
-          {/*  <div className="widget-forecast">*/}
-          {/*    <h5>{weatherForecastData.list[0].main.temp}°C</h5>*/}
-          {/*    <p className="forecast">Feels like {weatherForecastData.list[0].main.feels_like}°C</p>*/}
-          {/*    <img className="icon"*/}
-          {/*         alt="Image"*/}
-          {/*         src={`https://openweathermap.org/img/w/${weatherForecastData.list[0].weather[0].icon}.png`}/>*/}
-          {/*    <h5 className="center">{sunrise.toDateString()}</h5>*/}
-          {/*  </div>*/}
-
-          {/*  <div className="widget-forecast">*/}
-          {/*    <h5>{weatherForecastData.list[0].temp}°C</h5>*/}
-          {/*    <p className="forecast">Feels like {weatherForecastData.list[0].main.feels_like}°C</p>*/}
-          {/*    <img className="icon"*/}
-          {/*         alt="Image"*/}
-          {/*         src={`https://openweathermap.org/img/w/${weatherForecastData.list[0].weather[0].icon}.png`}/>*/}
-          {/*    <h5 className="center">{sunrise.toDateString()}</h5>*/}
-          {/*  </div>*/}
-          </div>
-
-          <div className="widget-right-mid">
-
-          </div>
-
-          <div className="widget-right-bottom">
-            <p className="center">Humidity: {weatherData.main.humidity}%</p>
-            <p className="center">Wind speed: {weatherData.wind.speed} m/s</p>
-            <p className="center">
-              Sunrise: {sunrise.toLocaleTimeString()}
-              &nbsp;&nbsp;&nbsp;
-              Sunset: {sunset.toLocaleTimeString()}
-            </p>
-          </div>
-
+        <div className="widget-bottom">
+          <p className="center">Humidity: {weatherData.main.humidity}%</p>
+          <p className="center">Wind speed: {weatherData.wind.speed} m/s</p>
+          <p className="center">
+            Sunrise: {sunrise.toLocaleTimeString()}
+            &nbsp;&nbsp;&nbsp;
+            Sunset: {sunset.toLocaleTimeString()}
+          </p>
         </div>
       </div>
 
